e1f38f7799e8fb8364c81309cfa9487794ce1304
Fábio Silva <fabiojdrsilva@gmail.com>
2019-04-30 11:13:27 +0100

    Correções

--- src/client/DropBoxClient.java
+++ src/client/DropBoxClient.java
@@ -1,4 +1,52 @@
 package client;
 
+
+import server.DropBoxFactoryRI;
+import util.rmisetup.SetupContextRMI;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DropBoxClient {
+
+    /**
+     * Context for connecting a RMI client to a RMI Servant
+     */
+    private SetupContextRMI contextRMI;
+    /**
+     * Remote interface that will hold the Servant proxy
+     */
+    private DropBoxFactoryRI dropBoxFactoryRI;
+
+    public static void main(String[] args) {
+        if (args != null && args.length < 2) {
+            System.err.println("usage: java [options] edu.ufp.sd.calculator.server.FactoryStub <rmi_registry_ip> <rmi_registry_port> <service_name>");
+            System.exit(-1);
+        } else {
+            //1. ============ Setup client RMI context ============
+            DropBoxClient hwc = new DropBoxClient(args);
+            //2. ============ Lookup service ============
+            hwc.lookupService();
+            //3. ============ Play with service ============
+            hwc.playService();
+        }
+    }
+
+    public DropBoxClient(String args[]) {
+        try {
+            //List ans set args
+            printArgs(args);
+            String registryIP = args[0];
+            String registryPort = args[1];
+            String serviceName = args[2];
+            //Create a context for RMI setup
+            contextRMI = new SetupContextRMI(this.getClass(), registryIP, registryPort, new String[]{serviceName});
+        } catch (RemoteException e) {
+            Logger.getLogger(DropBoxClient.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
 }
