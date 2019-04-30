dbb5ef799a26937a4a59dc2317596dd05170efc8
Daniel Pinto <danielpt128@gmail.com>
2019-04-30 11:57:19 +0100

    Print

--- src/server/DropBoxSessionRI.java
+++ src/server/DropBoxSessionRI.java
@@ -1,6 +1,8 @@
 package server;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface DropBoxSessionRI extends Remote {
+    public void print() throws RemoteException;
 }
