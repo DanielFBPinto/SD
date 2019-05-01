24927756ccfefeedbe03abfc3cdd260b0500c6cd
Fábio Silva <fabiojdrsilva@gmail.com>
2019-05-01 15:53:08 +0100

    Changes

--- src/client/DropBoxObserverRI.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface DropBoxObserverRI extends Remote {
-    public void update() throws RemoteException;
-}
--- src/server/DropBoxSessionRI.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface DropBoxSessionRI extends Remote {
-    public void print() throws RemoteException;
-}
