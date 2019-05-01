543773be04b7c421d135bd9a84d50b22505f69e1
Fábio Silva <fabiojdrsilva@gmail.com>
2019-05-01 17:25:12 +0100

    Cria pasta ao fazer register

--- src/server/DropBoxFactoryImpl.java
+++ src/server/DropBoxFactoryImpl.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -11,23 +13,24 @@ public class DropBoxFactoryImpl extends UnicastRemoteObject implements DropBoxFa
 
     @Override
     public DropBoxSessionRI register(String username, String password) throws RemoteException {
-        if(DB.users.containsKey(username)){
+        if (DB.users.containsKey(username)) {
             return null;
         }
-        User user=new User(username,password);
-        DB.users.put(username,user);
-        DropBoxSessionImpl sessionImpl=new DropBoxSessionImpl();
-        DB.session.put(username,sessionImpl);
+        User user = new User(username, password);
+        DB.users.put(username, user);
+        DropBoxSessionImpl sessionImpl = new DropBoxSessionImpl();
+        DB.session.put(username, sessionImpl);
+        System.out.println(System.getProperty("user.dir"));
+        new File(System.getProperty("user.dir") + "../../../../Users/" + username).mkdirs();
         return sessionImpl;
-
 
     }
 
     @Override
     public DropBoxSessionRI login(String username, String password) throws RemoteException {
-       if(DB.users.containsKey(username)){
-           return DB.session.get(username);
-       }
+        if (DB.users.containsKey(username)) {
+            return DB.session.get(username);
+        }
         return null;
     }
 }
